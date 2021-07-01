3d8be7ca2a0a87cc28ccc5527ff15aa6e7929a26
Woong <wjsdnd04@naver.com>
2021-07-01 12:40:18 +0900

    7/1 Woong commit

--- WebApplication_project_2/settings.py
+++ WebApplication_project_2/settings.py
@@ -34,7 +34,7 @@ while True:
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env_list('SECRET_KEY')
+SECRET_KEY = env_list['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
